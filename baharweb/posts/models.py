db231037f0fd4a0521533e239495d666830e1fcf
baharah <baharahsaddiqi@gmail.com>
2025-11-04 23:06:06 +0100

    add an author to Post

--- baharweb/posts/models.py
+++ baharweb/posts/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
     title=models.CharField(max_length=25)
     body=models.CharField(max_length=500)
+    auther =models.ForeignKey(User , on_delete=models.CASCADE)
     #this fun make the self title that we postet
     def __str__(self):
         return f"{self.title}"
